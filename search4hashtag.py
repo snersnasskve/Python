55425dd0fc12a460b7d1d9978c0b97693127a7ce
Karen van Eck <sners_nass@yahoo.co.uk>
2017-08-05 18:44:55 +0100

    twitter stuff

---
+++ search4hashtag.py
@@ -0,0 +1,14 @@
+# sudo apt-get update
+# sudo apt-get install python-pip
+# sudo pip install twython
+
+# won't work on the mac for whatever
+
+#this is not working code
+
+from twython import Twython
+twitter = Twython()
+
+statuses.filter()
+
+stream.statuses.filter(track='Harris')
---
+++ sendingAtweet.py
@@ -0,0 +1,18 @@
+# sudo apt-get update
+# sudo apt-get install python-pip
+# sudo pip install twython
+
+# won't work on the mac for whatever
+
+
+from twython import Twython
+twitter = Twython()
+
+C_KEY = "1XYEbHDiBoD5iHiw2LK70ZE5U"
+C_SECRET = "6mCfasKF4iNNrY4j60nm9Enlq7Ggb4AVs9xPloJT8xrEVUTgDy"
+A_TOKEN = "88620543-fPtczMK8OUVVek684JaNVR6SMsjDGbvvBS7VIymBO"
+A_SECRET = "0twTxMWtoK6DF6d4TC4tdOrRPnArTjM4IcOHrkbeoHgX0"
+
+api = Twython(C_KEY, C_SECRET, A_TOKEN, A_SECRET)
+
+api.update_status("Python is cool - if this tweet arrives, that is!")
