8afe3e8eff1b0236c8bbe1b61ea8a3227be2c0da
Karen van Eck <sners_nass@yahoo.co.uk>
2017-10-01 14:59:29 +0100

    secrests

--- twythonCallbacks.py
+++ twythonCallbacks.py
@@ -2,6 +2,10 @@ from twython import Twython
 from twython import TwythonStreamer
 twitter = Twython()
 
+C_KEY = "1XYEbHDiBoD5iHiw2LK70ZE5U"
+C_SECRET = "6mCfasKF4iNNrY4j60nm9Enlq7Ggb4AVs9xPloJT8xrEVUTgDy"
+A_TOKEN = "88620543-fPtczMK8OUVVek684JaNVR6SMsjDGbvvBS7VIymBO"
+A_SECRET = "0twTxMWtoK6DF6d4TC4tdOrRPnArTjM4IcOHrkbeoHgX0"
 
 #   Define a new class which extends TwythonStreamer
 class MyStreamer(TwythonStreamer):
