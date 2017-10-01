12a431fef64cfee979dfa49f8fcd78f3aa647802
Karen van Eck <sners_nass@yahoo.co.uk>
2017-10-01 14:55:11 +0100

    add import

--- twythonCallbacks.py
+++ twythonCallbacks.py
@@ -1,9 +1,14 @@
+
+
 #   Define a new class which extends TwythonStreamer
 class MyStreamer(TwythonStreamer):
     #Callback called on_success()
     def on_success(self, data):
         if 'text' in data:
             print("Found it!")
+
+from twython import Twython
+twitter = Twython()
 
 stream = MyStreamer(C_KEY, C_SECRET, A_TOKEN, A_SECRET)
 
