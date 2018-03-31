1dc2a3e687ec8028df8701f6488825f02844e08d
Karen pi <sners_nass@hotmail.com>
2018-03-31 20:31:01 +0000

    servo

---
+++ servoScanCodeEg.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+pwm = GPIO.PWM(12, 50)
+pwm.start(0)
+for i in range(100):
+    pwm.ChangeDutyCycle(i)
+    time.sleep(0.1)
+for i in range(100, 0, -1):
+    pwm.ChangeDutyCycle(i)
+    time.sleep(0.1)
+GPIO.cleanup()
